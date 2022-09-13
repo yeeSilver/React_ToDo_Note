84613858b87b48d64e303ec41efbeadae8ae3809
yeeun_An <an446@naver.com>
2022-09-13 20:19:30 +0900

    :sparkles: category Move Component add

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
+import styled from "styled-components";
 import { Categories, IToDo, toDoState } from "../atoms";
+
+const List = styled.li`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Item = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+`;
+
+const Deletebtn = styled.button`
+  border: none;
+  background-color: #e56852f9;
+  color: ivory;
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.9);
+  }
+`;
+
+const Movebtn = styled.button`
+  border: none;
+  background-color: #4289cbf8;
+  margin-right: 5px;
+  color: ivory;
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.9);
+  }
+`;
+
+const Category = styled.div`
+  display: flex;
+  flex-direction: center;
+`;
+const Categorybtn = styled.button`
+  background-color: #87baeaf8;
+  border-radius: 5px;
+  margin-left: 5px;
+  &:hover {
+    cursor: pointer;
+    background-color: #4289cbf8;
+    color: ivory;
+  }
+`;
 
 export default function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
+  const [openMove, setOpenMove] = useState(false);
+
+  const onMove = () => {
+    setOpenMove((prev) => !prev);
+  };
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
@@ -33,30 +87,40 @@ export default function ToDo({ text, category, id }: IToDo) {
       ];
     });
   };
+
   return (
-    <li>
-      <span>{text}</span>
-      {/* To_DO가 아니라면 TO_DO버튼 보여주기 */}
-      {category !== Categories.TO_DO && (
-        <button name={Categories.TO_DO} onClick={onClick}>
-          TO DO
-        </button>
-      )}
-      {category !== Categories.DOING && (
-        <button name={Categories.DOING} onClick={onClick}>
-          DOING
-        </button>
-      )}
-      {category !== Categories.DONE && (
-        <button name={Categories.DONE} onClick={onClick}>
-          DONE
-        </button>
-      )}
-      {category !== Categories.DELETE && (
-        <button name={Categories.DELETE} onClick={onDelete}>
-          DELTE
-        </button>
-      )}
-    </li>
+    <List>
+      <Item>
+        <span>{text}</span>
+        <div>
+          <Movebtn onClick={onMove}>MOVE</Movebtn>
+          {category !== Categories.DELETE && (
+            <Deletebtn name={Categories.DELETE} onClick={onDelete}>
+              DELTE
+            </Deletebtn>
+          )}
+        </div>
+      </Item>
+      {openMove ? (
+        <Category>
+          {/* To_DO가 아니라면 TO_DO버튼 보여주기 */}
+          {category !== Categories.TO_DO && (
+            <Categorybtn name={Categories.TO_DO} onClick={onClick}>
+              TO DO
+            </Categorybtn>
+          )}
+          {category !== Categories.DOING && (
+            <Categorybtn name={Categories.DOING} onClick={onClick}>
+              DOING
+            </Categorybtn>
+          )}
+          {category !== Categories.DONE && (
+            <Categorybtn name={Categories.DONE} onClick={onClick}>
+              DONE
+            </Categorybtn>
+          )}
+        </Category>
+      ) : null}
+    </List>
   );
 }
